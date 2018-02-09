1db35bcc63869b6f5c85d9d37d683bb5897358ec
epicemaraty <realnuaimi@mail.csuchico.edu>
2018-02-08 19:45:35 -0800

    Get user input and ASCII Code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Rashed Alnuaimi
 #include<iostream>
 
 using namespace std;
@@ -6,19 +6,25 @@ using namespace std;
 int main()
 {
 
+char letter;
+
   cout<<"What character do you want to know about?\n";
 
+cin >> letter;
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
 
-  //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
-
-  //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
+  if ( letter >= 'A' && letter <= 'Z' ) {
+  cout<<letter<< " is an upper case letter!\n";
+} else if ( letter >= 'a' && letter <= 'z' ){
   
+  //when user's entry is between a-z...
+  cout<<letter<<" is a lower case letter!\n";
+} else {
+  //in all other cases...
+  cout<<letter<<"?! Pssh. What are you talking about?\n";
+  }
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int)letter<<"\n";
 
   return 0;
 }
